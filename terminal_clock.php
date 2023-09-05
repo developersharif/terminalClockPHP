becdc150e83d59d1394ee40d10ef1965c80531e8
Sharif <developersharif@yahoo.com>
2023-09-05 21:01:25 +0600

    formated

--- terminal_clock.php
+++ terminal_clock.php
@@ -1,61 +1,62 @@
 <?php
-function os_platform($callback,$os="WIN"){
+function os_platform($callback, $os = "WIN")
+{
     if (strtoupper(substr(PHP_OS, 0, 3)) === $os) {
         $callback();
     }
 }
-function get_terminal_width() {
-    if(strtoupper(substr(PHP_OS, 0, 3)) === "WIN"){
+function get_terminal_width()
+{
+    if (strtoupper(substr(PHP_OS, 0, 3)) === "WIN") {
         $output = shell_exec('mode con');
         if (preg_match('/Columns:\s+(\d+)/', $output, $matches)) {
-            return (int)$matches[1];
+            return (int) $matches[1];
         }
-    }elseif(strtoupper(substr(PHP_OS, 0, 3)) === "LIN"){
+    } elseif (strtoupper(substr(PHP_OS, 0, 3)) === "LIN") {
         return exec('tput cols');
     }
 }
-function console_style($text,$color=null,$bg=null,$fontSize=55){
+function console_style($text, $color = null, $bg = null, $fontSize = 55)
+{
 
-    os_platform(function() use ($text,$color,$bg,$fontSize){
-        
+    os_platform(function () use ($text, $color, $bg, $fontSize) {
+
         $fontSizeCode = "\e[{$fontSize}m";
 
 // Get the terminal width
-$terminalWidth = get_terminal_width();
+        $terminalWidth = get_terminal_width();
 
 // Calculate the padding on each side to center the text
-$padding = str_repeat(' ', floor(($terminalWidth - strlen($text)) / 2));
+        $padding = str_repeat(' ', floor(($terminalWidth - strlen($text)) / 2));
 
-$formattedText = "\e[1m" . $fontSizeCode . $padding . $text . $padding . "\e[0m";
+        $formattedText = "\e[1m" . $fontSizeCode . $padding . $text . $padding . "\e[0m";
 
-echo $color.$formattedText.$bg."\n";
-    sleep(1);
-    echo chr(27).chr(91).'H'.chr(27).chr(91).'J';
-    },"WIN");
-    os_platform(function() use ($text,$color,$bg,$fontSize) {
-        
-            // Linux or other Unix-like system
-    $fontSizeCode = "\e[{$fontSize}m";
+        echo $color . $formattedText . $bg . "\n";
+        sleep(1);
+        echo chr(27) . chr(91) . 'H' . chr(27) . chr(91) . 'J';
+    }, "WIN");
+    os_platform(function () use ($text, $color, $bg, $fontSize) {
+
+        // Linux or other Unix-like system
+        $fontSizeCode = "\e[{$fontSize}m";
 
 // Get the terminal width
-$terminalWidth = get_terminal_width();
+        $terminalWidth = get_terminal_width();
 
 // Calculate the padding on each side to center the text
-$padding = str_repeat(' ', floor(($terminalWidth - strlen($text)) / 2));
+        $padding = str_repeat(' ', floor(($terminalWidth - strlen($text)) / 2));
 
-$formattedText = "\e[1m" . $fontSizeCode . $padding . $text . $padding . "\e[0m";
+        $formattedText = "\e[1m" . $fontSizeCode . $padding . $text . $padding . "\e[0m";
 
-echo $color.$formattedText.$bg."\n";
-sleep(1);
-system("clear");
+        echo $color . $formattedText . $bg . "\n";
+        sleep(1);
+        system("clear");
 
-    },"LIN");
+    }, "LIN");
 
 }
 
-do{
-$text = date("d-M-Y h:i:s a");
-console_style($text,"\e[38;5;231m","\e[48;5;196m",500);
-}while(true);
-
-
+do {
+    $text = date("d-M-Y h:i:s a");
+    console_style($text, "\e[38;5;231m", "\e[48;5;196m", 500);
+} while (true);
